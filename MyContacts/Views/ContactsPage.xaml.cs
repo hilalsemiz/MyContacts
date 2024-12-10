666d863ef4b907dbbd4529d2e0ef9d81d8c32d17
hilalsemiz <106753104+hilalsemiz@users.noreply.github.com>
2024-12-10 12:46:22 +0300

    Update ContactsPage.xaml.cs

--- MyContacts/Views/ContactsPage.xaml.cs
+++ MyContacts/Views/ContactsPage.xaml.cs
@@ -10,9 +10,26 @@ public partial class ContactsPage : ContentPage
     public ContactsPage()
     {
 		InitializeComponent();
+                ContactsList.ItemsSource = repository.GetAllContacts(); 
 
         
     }
+     private void ContactsList_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+    {
+        var selectedContact = e.SelectedItem as ContactInfo;
+        if (selectedContact != null)
+        {
+            
+            Shell.Current.GoToAsync(nameof(EditContactPage) + "?id=" + selectedContact.Id);
+        }
+    }
+
+    
+    private async void AddContactButton_Clicked(object sender, EventArgs e)
+    {
+        await Shell.Current.GoToAsync(nameof(AddContactPage));
+    }
+}
 
     protected override void OnAppearing()
     {
