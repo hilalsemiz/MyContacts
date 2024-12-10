714da426fd1818b0326354c41c357b148424bc43
hilalsemiz <106753104+hilalsemiz@users.noreply.github.com>
2024-12-10 12:47:42 +0300

    Update App.xaml.cs

--- MyContacts/App.xaml.cs
+++ MyContacts/App.xaml.cs
@@ -9,4 +9,15 @@
             MainPage = new AppShell();
         }
     }
+    public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+
+        Routing.RegisterRoute(nameof(ContactsPage), typeof(ContactsPage));
+        Routing.RegisterRoute(nameof(AddContactPage), typeof(AddContactPage));
+        Routing.RegisterRoute(nameof(EditContactPage), typeof(EditContactPage));  // EditContactPage için yönlendirme ekleniyor
+    }
+}
 }
